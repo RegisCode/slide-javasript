077423a29c5cd83b150ae25153104c83ec571e42
RegisCode <xbiruleix@gmail.com>
2024-03-25 23:00:44 -0300

    adicionado a paginacao por slide

--- js/script.js
+++ js/script.js
@@ -3,3 +3,4 @@ import {Slide, SlideNav} from './slide.js';
 const slide = new SlideNav('.slide', '.slide-wrapper'); // Ajuste aqui para '.slide' e '.slide-wrapper'
 slide.init();
 slide.addArrow('.prev', '.next');
+slide.addControl();
